a3c87fe88eb6faadded0f7b37d6903e8e9560342
Daryl <Daryl@172-16-20-37.DYNAPOOL.NYU.EDU>
2015-02-04 22:42:30 -0500

    Quiz App
    Quiz App tutorial I created from a tutorial online

---
+++ Quiz App/Quiz/ViewController.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Daryl Dalman on 2/4/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+
+IBOutlet UILabel *label;
+}
+
+-(IBAction)answer1:(id)sender;
+
+-(IBAction)answer2:(id)sender;
+
+-(IBAction)answer3:(id)sender;
+
+@end
+
