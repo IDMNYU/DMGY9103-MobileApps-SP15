4627f3449e4440e12564df4cadf5e375c11b4ce3
Daryl <Daryl@Daryls-MacBook-Pro.local>
2015-02-23 01:34:56 -0500

    HypnoNerd chapter 6
    Chapter 6 code from the iOS programming book

---
+++ HypnoNerd/HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Student on 2/22/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
